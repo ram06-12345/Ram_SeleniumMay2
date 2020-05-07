f715854271cee853448f45e9812d240105995ba8
ram06-12345 <64738023+ram06-12345@users.noreply.github.com>
2020-05-07 22:24:19 +0530

    Add files via upload

---
+++ Week 1/Day 2/Classroom/Arrays/FindSecondLargest.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+import java.util.Arrays;
+
+public class FindSecondLargest {
+	
+	public static void main(String[] args) {
+		int[] data = {3,2,11,4,6,7};
+		
+		Arrays.sort(data);
+		
+		for (int i = 0; i < data.length; i++) {
+			
+		}
+		
+		System.out.println(data[data.length-2]);
+	}
+
+}
